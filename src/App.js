666511ea00656ee7cbbbfbf75b10afa32bf754fa
QiyangGeng <75600357+QiyangGeng@users.noreply.github.com>
2023-01-16 22:22:38 -0800

    Test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import {Container, Typography} from "@mui/material";
+import {Container, Paper, Typography} from "@mui/material";
 import ResponsiveAppBar from "./ResponsiveAppBar";
 
 function App() {
   return (
-  <Container sx={{ color: "background.default"}} disableGutters>
-      <ResponsiveAppBar />
-      <Typography variant="h1" sx={{ my: 4, textAlign: "center", color: "primary.main"}}>
-          Hello World!
-      </Typography>
-  </Container>
+      <Container sx={{ color: "background.default"}} maxWidth={false} disableGutters>
+          <ResponsiveAppBar />
+          <Container sx={{ color: "background.default"}} maxWidth={"lg"}>
+              <Typography variant="h2" sx={{ my: 4, textAlign: "center", color: "primary.main"}}>
+                  Hello World!
+              </Typography>
+              <Typography sx={{ my: 4, textAlign: "left", color: "primary.main"}}>
+                  Welcome to my website!
+              </Typography>
+              <Paper elevation={6} sx={{ maxHeight: "false"}}>
+                  <Typography variant="h3" sx={{ my: 4, textAlign: "left", color: "primary.main"}}>
+                      Hello World!
+                  </Typography>
+              </Paper>
+          </Container>
+      </Container>
   );
 }
 
