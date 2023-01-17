a6ce43a36cc9a317dcea63efcae9c98768282a01
QiyangGeng <75600357+QiyangGeng@users.noreply.github.com>
2023-01-16 21:56:50 -0800

    Test

--- src/ResponsiveAppBar.js
+++ src/ResponsiveAppBar.js
@@ -28,23 +28,7 @@ function ResponsiveAppBar() {
         <AppBar position="static">
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
-                    <GitHubIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                    </Typography>
+                    <GitHubIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 3 }} href=""/>
 
                     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
                         <IconButton
